76cd098d2619152aa6603fd481d4f3b94bc8508d
Apoorv Garg <u.garg.10@gmail.com>
2020-03-16 17:45:48 +0530

    First Commit

---
+++ Assets/Script/PlayerMvmt.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMvmt : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce,sideForce;
+    private Scene scene;
+    bool score;
+    public Text scoreText;
+    float time = 0;
+    int scoreTime;
+    int penalty = 0;
+
+    public GameObject levelCompleteUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        forwardForce=1000f;
+        sideForce = 50f;
+        score = true;
+        scene = SceneManager.GetActiveScene();
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle");
+            if (scoreTime - 3 > 0)
+            {
+                penalty += 3;
+                col.collider.GetComponent<Collider>().enabled = !col.collider.GetComponent<Collider>().enabled;
+            }
+            else
+            {
+                
+                SceneManager.LoadScene(scene.name);
+            }
+        }
+        else if (col.collider.tag == "Net")
+        {
+            Debug.Log("We fell from the plane");
+            SceneManager.LoadScene(scene.name);
+        }
+        else if (col.collider.tag == "Finish")
+        {
+            score = false;
+            Debug.Log("Level Complete");
+            levelCompleteUI.SetActive(true);
+        }
+    }
+
+    void Movenment()
+    {
+        if (Input.GetKey("d")){
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+        Movenment();
+        if (score)
+        {
+            time += Time.deltaTime;
+            scoreTime = (int)time - penalty;
+            scoreText.text = scoreTime.ToString();
+        }
+    }
+}
